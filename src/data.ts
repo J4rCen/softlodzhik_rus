728c24b0ab309a36368eacd617892cdcec95ad82
danil <d_davletov@internet.ru>
2025-03-11 22:22:35 +0100

    fix

---
+++ src/data.ts
@@ -0,0 +1,110 @@
+export const data = [
+    {
+        title: 'test',
+        image: '/image1.png',
+        id: 1,
+        fileType:  "photo",
+        quantity: 12,
+        dateOfAddition: '21.01.2002',
+        tags: [
+            {
+                name: "Лес",
+                color: 'green'
+            },
+            {
+                name: "Кадры",
+                color: 'orange'
+            }
+        ]
+    },
+    {
+        title: 'test',
+        image: '/image2.png',
+        id: 2,
+        fileType:  "video",
+        quantity: 12,
+        dateOfAddition: '21.01.2002',
+        tags: [
+            {
+                name: "Лес",
+                color: 'green'
+            },
+            {
+                name: "Кадры",
+                color: 'orange'
+            }
+        ]
+    },
+    {
+        title: 'test',
+        image: '/image1.png',
+        id: 3,
+        fileType:  "photo",
+        quantity: 12,
+        dateOfAddition: '21.01.2002',
+        tags: [
+            {
+                name: "Лес",
+                color: 'green'
+            },
+            {
+                name: "Кадры",
+                color: 'orange'
+            }
+        ]
+    },
+    {
+        title: 'test',
+        image: '/image2.png',
+        id: 4,
+        fileType:  "video",
+        quantity: 12,
+        dateOfAddition: '21.01.2002',
+        tags: [
+            {
+                name: "Лес",
+                color: 'green'
+            },
+            {
+                name: "Пролеты",
+                color: 'red'
+            }
+        ]
+    },
+    {
+        title: 'test',
+        image: '/image1.png',
+        id: 5,
+        fileType:  "photo",
+        quantity: 12,
+        dateOfAddition: '21.01.2002',
+        tags: [
+            {
+                name: "Пролеты",
+                color: 'red'
+            },
+            {
+                name: "Кадры",
+                color: 'orange'
+            }
+        ]
+    },
+    {
+        title: 'test',
+        image: '/image1.png',
+        id: 6,
+        fileType:  "video",
+        quantity: 12,
+        dateOfAddition: '21.01.2002',
+        tags: [
+            {
+                name: "Лес",
+                color: 'green'
+            },
+            {
+                name: "Пролеты",
+                color: 'red'
+            }
+        ]
+    }
+]
